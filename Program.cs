2c7452dd60226c9936c7a81d1d2b71a42134a2d0
Andrey <kaverin97andry@mail.ru>
2022-05-18 16:36:37 +0300

    Add HOMETASK

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int number = new Random().Next(100,1000);
+string newNumber = number.ToString();
+Console.WriteLine($"{number} -> {newNumber[1]}");
+
